551cd08b1f37ec53e401b5bbf2eb870716c44fe8
Victor Franco <vfrancohdez1@hotmail.com>
2021-12-15 10:25:56 -0600

    chore: fix code formatting and change skew color

--- web/js/skewed.js
+++ web/js/skewed.js
@@ -1,6 +1,6 @@
-const skew = document.querySelector(".skewed");
+const skew = document.querySelector(".skewed")
 window.addEventListener('scroll', function(){
-    let value = -10 + window.scrollY/20;
-    if (value > 0) value = 0;
-    skew.style.transform = `skewY(${value}deg)`;
+    let value = -10 + window.scrollY/20
+    if (value > 0) value = 0
+    skew.style.transform = `skewY(${value}deg)`
 });

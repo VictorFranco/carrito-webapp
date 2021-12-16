76e970857223708fd0613a0ff2153e1d96d4afcb
Victor Franco <vfrancohdez1@hotmail.com>
2021-12-15 19:29:23 -0600

    feat(AddToCart): save cart array

--- src/java/cart/AddToCart.java
+++ src/java/cart/AddToCart.java
@@ -6,11 +6,13 @@
 package cart;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import data.*;
+import java.util.ArrayList;
 
 /**
  *
@@ -21,6 +23,21 @@ public class AddToCart extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-    }
 
+        HttpSession session=request.getSession();
+        int id = Integer.parseInt(request.getParameter("id"));
+        Product[] products = (Product[]) session.getAttribute("products");
+        Product product = products[id];
+        int quantity = Integer.parseInt(request.getParameter("quantity"));
+
+        ArrayList<Product> cart = (ArrayList<Product>) session.getAttribute("cart");
+        if (cart == null) cart = new ArrayList<Product>();
+
+        for (int i=0; i<quantity; i++) cart.add(product);
+
+        session.setAttribute("cart",cart); // update cart
+
+        response.sendRedirect(request.getContextPath() + "/index.jsp");
+
+    }
 }
